5f107cad4d8a26299826cb142d87401f9290deb3
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-07 14:28:43 +0100

    feat-bonus: added the ability to switch the bulb on and off

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@ const btnElem = document.querySelector(".switch-btn");
 const lightImg = document.querySelector(".light");
 
 btnElem.addEventListener("click", function() {
-    lightImg.src = "../img/yellow_lamp.png"
-})
+    
+    if (lightImg.src.includes("yellow")) {
+        btnElem.innerHTML = "Accendi";
+        lightImg.src = "../img/white_lamp.png"
+    } else {
+        btnElem.innerHTML = "Spegni";
+        lightImg.src = ".//img/yellow_lamp.png"
+    }
+});
