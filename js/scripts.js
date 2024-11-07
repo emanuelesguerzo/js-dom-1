d827f2a98390e9cb227ec6cf0fc673230ed6b0c2
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-07 13:32:23 +0100

    feat: added the ability to switch on the lightbulb

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const btnElem = document.querySelector(".switch-btn");
+const lightImg = document.querySelector(".light");
+
+btnElem.addEventListener("click", function() {
+    lightImg.src = "../img/yellow_lamp.png"
+})
